7a5f60ac77090375265841406b6a1b24f8e3ab87
Eduardo Verdusco <em.verdusco@gmail.com>
2020-12-27 22:05:31 -0800

    testing react

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "index.html";
+
+const rootElement = document.getElementById("root");
+React.ReactDOM.render(
+    <Main />, rootElement
+)
