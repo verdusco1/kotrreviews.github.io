c780c06adeaa3e9b945cf2a63277b2c1d3b1117d
Eduardo Verdusco <em.verdusco@gmail.com>
2021-01-01 22:31:15 -0800

    updated homepage

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "index.html";
-
-const rootElement = document.getElementById("root");
-React.ReactDOM.render(
-    <Main />
-    <App />, rootElement
-)
